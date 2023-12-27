f609d941fad884b8dbbf901eef0c043b76c335ef
Danu Tryas <Danupristowo09@gmail.com>
2023-12-27 21:48:46 +0700

    change project font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      sans: ["Lexend Deca"],
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
